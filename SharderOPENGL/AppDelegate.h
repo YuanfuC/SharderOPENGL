fffa71bd2a8190022dd2e697050ffe236967ee25
ChenYuanfu <chenyuanfu@zerozero.cn>
2018-11-18 17:54:18 +0800

    Initial Commit

---
+++ SharderOPENGL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SharderOPENGL
+//
+//  Created by ChenYuanfu on 2018/11/18.
+//  Copyright © 2018年 Zerozero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SharderOPENGL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SharderOPENGL
+//
+//  Created by ChenYuanfu on 2018/11/18.
+//  Copyright © 2018年 Zerozero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
